c2f71d7fd9b4cff6b97f1f7e7e9f33900d1add9e
vinc32 <vinc32@gmx.net>
2013-03-15 02:05:02 +0100

    Update BoardConfig.mk for Razr I - ImageSize?!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,15 +2,15 @@
 -include device/motorola/msm8960-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
--include vendor/motorola/scorpion_mini/BoardConfigVendor.mk
+-include vendor/motorola/smi/BoardConfigVendor.mk
 
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 4294967296
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/scorpion_mini/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/smi/bluetooth
 
 # Kernel
-#TARGET_PREBUILT_KERNEL := device/motorola/scorpion_mini/kernel
+#TARGET_PREBUILT_KERNEL := device/motorola/smi/kernel
 
 #Twrp
 DEVICE_RESOLUTION := 540x960
