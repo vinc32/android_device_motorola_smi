eea50bb41a72fd4adbd3e7654807f995e3f09974
vinc32 <vinc32@gmx.net>
2013-03-15 03:34:00 +0100

    update vendor setup.sh for Razr I

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-add_lunch_combo aokp_scorpion_mini-eng
-add_lunch_combo cm_scorpion_mini-userdebug
-add_lunch_combo cna_scorpion_mini-eng
-add_lunch_combo full_scorpion_mini-eng
+add_lunch_combo aokp_smi-eng
+add_lunch_combo cm_smi-userdebug
+add_lunch_combo cna_smi-eng
+add_lunch_combo full_smi-eng
