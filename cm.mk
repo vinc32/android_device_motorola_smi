7fa96774cf7934f77104d2fcab8be2b781fd9ea7
vinc32 <vinc32@gmx.net>
2013-03-15 02:41:49 +0100

    update cm.mk - needs to be overlooked

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/motorola/scorpion_mini/full_scorpion_mini.mk)
+$(call inherit-product, device/motorola/smi/full_smi.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/cdma.mk)
@@ -9,14 +9,14 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/motorola/scorpion_mini/overlay/cm
+DEVICE_PACKAGE_OVERLAYS += device/motorola/smi/overlay/cm
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=scorpion_mini TARGET_DEVICE=scorpion_mini BUILD_FINGERPRINT="motorola/XT907_verizon/scorpion_mini:4.1.1/9.8.1Q_25/35:user/release-keys" PRIVATE_BUILD_DESC="smq_vzw-user 4.1.1 9.8.1Q_25 35 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=XT890_rtcoreeu TARGET_DEVICE=smi BUILD_FINGERPRINT="motorola/XT890_motorola/smi:4.2.1/9.8.2I-50_SMI-26:user/release-keys" PRIVATE_BUILD_DESC="XT890_emara-user 4.1.2 9.8.2I-50_SMI-26 1358465787 release-keys"
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
-PRODUCT_NAME := cm_scorpion_mini
-PRODUCT_DEVICE := scorpion_mini
+PRODUCT_NAME := XT890_rtcoreeu
+PRODUCT_DEVICE := smi
 
