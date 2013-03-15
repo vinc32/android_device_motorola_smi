545dad23e9aaec6ba89e4396dbf893fbf0161bdb
vinc32 <vinc32@gmx.net>
2013-03-15 02:03:26 +0100

    Update AndroidProducts for Razr I

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_scorpion_mini.mk
-ifeq ($(TARGET_PRODUCT),aokp_scorpion_mini)
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_smi.mk
+ifeq ($(TARGET_PRODUCT),aokp_smi)
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
 endif
-ifeq ($(TARGET_PRODUCT),cna_scorpion_mini)
+ifeq ($(TARGET_PRODUCT),cna_smi)
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/cna.mk
 endif
